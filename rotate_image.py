9734b455dd321f20f2e64b023eb2350716fcb370
Conrad Clement <oconradh@gmail.com>
2021-06-25 01:01:26 -0600

    Rotate Image

--- rotate_image.py
+++ rotate_image.py
@@ -12,5 +12,5 @@ class Solution:
     def reflect(self, matrix):
         n = len(matrix)
         for i in range(n):
-            for j in range(n // 2):
-                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+            for j in range(n//2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
